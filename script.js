1159c521d7e9f9af3e728ad3ad919b67a22fc981
mlacerda97 <44003311+mlacerda97@users.noreply.github.com>
2022-07-19 17:23:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.body.addEventListener('keyup', (event) => { // Reconhecer os botões para teclar
+document.body.addEventListener('keyup', (event) => { 
     playSound(event.code.toLowerCase())
 }); 
 
-document.querySelector('.composer button').addEventListener('click', () => { //adicionar ação no botão Tocar
+document.querySelector('.composer button').addEventListener('click', () => { 
     let song = document.querySelector('#input').value;
 
     if(song !== '') {
@@ -12,19 +12,19 @@ document.querySelector('.composer button').addEventListener('click', () => { //a
 });
 
 function playSound(sound) {
-    let audioElement = document.querySelector(`#s_${sound}`); // Selecionar os audios
+    let audioElement = document.querySelector(`#s_${sound}`); 
     let keyElement = document.querySelector(`div[data-key="${sound}"]`)
 
-    if(audioElement) { //Verificar que não vai clicar em letras que não tem som
-        audioElement.currentTime = 0; // não espera os sons acabarem pra tocar novamente
-        audioElement.play(); //play nas teclas
+    if(audioElement) { 
+        audioElement.currentTime = 0; 
+        audioElement.play(); 
     }
 
     if(keyElement) {
-        keyElement.classList.add('active'); // adicionar borda amarela
+        keyElement.classList.add('active');
 
         setTimeout(() => {
-            keyElement.classList.remove('active'); // remover borda amarela
+            keyElement.classList.remove('active'); 
         }, 300);
     }
 }
@@ -43,7 +43,7 @@ function playComposition(songArray) {
 
 let clickElement = document.querySelectorAll('.key');
 
-document.querySelectorAll('.key').forEach(element => { // PARA CLIQUE NA TELA, USADO FOREACH
+document.querySelectorAll('.key').forEach(element => { 
     element.addEventListener('click', () => {
       const clickedKey = element.getAttribute('data-key')
       playSound(clickedKey)
